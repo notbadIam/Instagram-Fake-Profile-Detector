33816f9fbb6332418625b12e86311eced0310673
Aman <164004893+notbadIam@users.noreply.github.com>
2025-02-15 21:20:36 +0530

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -x
+apt-get update
+apt-get install -y wget unzip
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+apt-get update
+apt-get install -y google-chrome-stable
